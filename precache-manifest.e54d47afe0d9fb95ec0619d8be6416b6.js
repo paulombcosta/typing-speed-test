5d70f4b1768e08251661a327c7f54a7e6c080861
Paulo Costa <paulombcosta@gmail.com>
2021-08-22 02:42:19 +0200

    Updates

---
+++ precache-manifest.e54d47afe0d9fb95ec0619d8be6416b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/typing-speed-test/favicon.ico"
+  },
+  {
+    "revision": "1c4fb55a3a66f4317780b4aa68b1989f",
+    "url": "/typing-speed-test/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/typing-speed-test/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/typing-speed-test/manifest.json"
+  },
+  {
+    "revision": "5107286669329061d2e6",
+    "url": "/typing-speed-test/static/css/main.4057af5e.chunk.css"
+  },
+  {
+    "revision": "5107286669329061d2e6",
+    "url": "/typing-speed-test/static/js/main.51072866.chunk.js"
+  },
+  {
+    "revision": "aebeec767fe4af500853",
+    "url": "/typing-speed-test/static/js/runtime~main.aebeec76.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/typing-speed-test/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
